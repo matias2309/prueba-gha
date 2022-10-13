e3496b253d95ba2efc4371871dd5230d5e83fec9
Matias Aros <maros@baufest.com>
2022-10-13 13:45:38 -0300

    comentario.

--- tests/registro.spec.ts
+++ tests/registro.spec.ts
@@ -2,4 +2,15 @@ import { test, expect, type Page } from '@playwright/test';
 
 test('Validar ingresado Registro', async ({ page }) => {
     await page.goto('https://buggy.justtestit.org/register');
+  // Click [placeholder="Login"]
+  await page.locator('[placeholder="Login"]').click();
+  await page.locator('[placeholder="Login"]').fill('matiasAros');
+  // Click text=Login Register >> input[name="password"]
+  await page.locator('text=Login Register >> input[name="password"]').click();
+  await page.locator('text=Login Register >> input[name="password"]').fill('A86941818m"#');
+
+ 
+  // Close page
+  await page.close();
   });
+  
